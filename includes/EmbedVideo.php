367a58c0aefd59435ec4df5f268813ce4dd97846
Madness <daniele@l33t.it>
2025-07-21 11:50:41 -0700

    Test polyfill

--- includes/EmbedVideo.php
+++ includes/EmbedVideo.php
@@ -17,6 +17,22 @@ use MediaWiki\Parser\Parser;
 use MediaWiki\Parser\PPFrame;
 use RuntimeException;
 
+$classAliasMap = [
+	'1.44' => [
+		[ 'RepoGroup', 'MediaWiki\\FileRepo\\RepoGroup' ],
+	]
+];
+
+foreach ( $classAliasMap as $version => $classMap ) {
+	if ( version_compare( MW_VERSION, $version, '<' ) ) {
+		foreach ( $classMap as $classAlias ) {
+			if ( !class_exists( $classAlias[0] ) && class_exists( $classAlias[1] ) ) {
+				class_alias( $classAlias[0], $classAlias[1] );
+			}
+		}
+	}
+}
+
 class EmbedVideo {
 	/**
 	 * @var Parser|null
