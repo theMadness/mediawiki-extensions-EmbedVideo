1324310493fc86c90967388291e4980afbf2fc36
Madness <daniele@l33t.it>
2025-07-21 14:31:07 -0700

    Corrected class map alias.

--- includes/Bootstrap.php
+++ includes/Bootstrap.php
@@ -1,20 +1,19 @@
 <?php
 
-
 namespace MediaWiki\Extension\EmbedVideo;
 
 class Bootstrap {
     public static function onRegistration() {
         $classAliasMap = [
             '1.44' => [
-                [ 'MediaWiki\\FileRepo\\RepoGroup', 'RepoGroup' ],
+                [ 'RepoGroup', 'MediaWiki\\FileRepo\\RepoGroup' ],
             ]
         ];
 
         foreach ( $classAliasMap as $version => $classMap ) {
             if ( version_compare( MW_VERSION, $version, '<' ) ) {
                 foreach ( $classMap as $classAlias ) {
-                    if ( !class_exists( $classAlias[0] ) && class_exists( $classAlias[1] ) ) {
+                    if ( class_exists( $classAlias[0] ) && !class_exists( $classAlias[1] )) {
                         class_alias( $classAlias[0], $classAlias[1] );
                     }
                 }
